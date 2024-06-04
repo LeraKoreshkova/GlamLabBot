6d9d53aa3d2362229983300e6859a8afb8979e2b
LeraKoreshkova <lerakoreshkova04@gmail.com>
2024-06-04 12:26:20 +0800

    Add files via upload

---
+++ common/bot_cmnds_list.py
@@ -0,0 +1,9 @@
+from aiogram.types import BotCommand
+
+
+private = [
+    BotCommand(command='catalog', description='Посмотреть каталог'),
+    BotCommand(command='about', description='О нас'),
+    BotCommand(command='payment', description='Варианты оплаты'),
+    BotCommand(command='shipping', description='Варианты доставки')
+]
